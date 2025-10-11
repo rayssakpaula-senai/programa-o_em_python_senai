2dc6f589a50a3db54138b4e77ba0031d9cbafa2a
Rayssa <rayssakemylly279@gmail.com>
2025-10-11 17:13:33 -0300

    Add files via upload

--- aula.py
+++ aula.py
@@ -1,67 +1,23 @@
-# Multiplicação de 4 por 6 e divisão do resultado por 2
-resultado_multiplicacao_divisao = (4 * 6) / 2
-print("Multiplicação e divisão:", resultado_multiplicacao_divisao)
+# Usando range() para gerar números pares de 2 a 20 e imprimindo-os
+for num in range(2, 21, 2):
+    print(num)
 
-# Soma de 5 e 7
-resultado_soma = 5 + 7
-print("Soma:", resultado_soma)
+# Criando a lista 'numeros' com inteiros de 1 a 10 e imprimindo-a
+numeros = list(range(1, 11))
+print(numeros)
 
-# Elevar 3 à potência de 4
-resultado_potencia = 3 ** 4
-print("Potência:", resultado_potencia)
+# Imprimindo o terceiro elemento da lista 'numeros'
+print(numeros[2])
 
-# Média de 8, 12 e 15
-resultado_media = (8 + 12 + 15) / 3
-print("Média:", resultado_media)
+# Adicionando o número 9 à lista 'numeros' e imprimindo a lista atualizada
+numeros.append(9)
+print(numeros)
 
-# Subtrair 10 de 2 e multiplicar o resultado por 5
-resultado_subtracao_multiplicacao = (2 - 10) * 5
-print("Subtração e multiplicação:", resultado_subtracao_multiplicacao)
+# Removendo o número 5 da lista 'numeros' e imprimindo a lista resultante
+numeros.remove(5)
+print(numeros)
 
-# Dividir 27 por 3 e somar 5
-resultado_divisao_soma = (27 / 3) + 5
-print("Divisão e soma:", resultado_divisao_soma)
-
-import math  # Para a função sqrt
-
-# Calcular o módulo de 17 por 4
-resultado_modulo = 17 % 4
-print("Módulo de 17 por 4:", resultado_modulo)
-
-# Multiplicar 9 por 3 e depois elevar o resultado ao quadrado
-resultado_multiplicacao_potencia = (9 * 3) ** 2
-print("Multiplicação e potência:", resultado_multiplicacao_potencia)
-
-# Calcular a raiz quadrada de 81
-resultado_raiz_quadrada = math.sqrt(81)
-print("Raiz quadrada de 81:", resultado_raiz_quadrada)
-
-# Adicionar 20 a 3 vezes 4
-resultado_adicao = 20 + (3 * 4)
-print("Adição com multiplicação:", resultado_adicao)
-
-# Multiplicar 15 por 2 e depois subtrair 7
-resultado_multiplicacao_subtracao = (15 * 2) - 7
-print("Multiplicação e subtração:", resultado_multiplicacao_subtracao)
-
-# Elevar 5 ao cubo
-resultado_cubo = 5 ** 3
-print("5 ao cubo:", resultado_cubo)
-
-# Calcular a média de 17, 21 e 25
-resultado_media_3 = (17 + 21 + 25) / 3
-print("Média de 17, 21 e 25:", resultado_media_3)
-
-# Multiplicar 11 por 2 e adicionar 7
-resultado_multiplicacao_adicao = (11 * 2) + 7
-print("Multiplicação e adição:", resultado_multiplicacao_adicao)
-
-# Subtrair 15 de 3 vezes 8 e dividir o resultado por 2
-resultado_subtracao_divisao = ((3 * 8) - 15) / 2
-print("Subtração e divisão:", resultado_subtracao_divisao)
-
-# Elevar 2 à potência de 10
-resultado_potencia_2 = 2 ** 10
-print("2 à potência de 10:", resultado_potencia_2)
-
-
+# Criando a lista 'carros' e concatenando-a com a lista 'numeros'
+carros = ['Fusca', 'Civic', 'Corolla']
+resultado = carros + numeros
+print(resultado)
