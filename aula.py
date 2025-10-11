43de97f7b0eb4589e7a4cf54bb7775d574248073
Rayssa <rayssakemylly279@gmail.com>
2025-10-11 12:09:00 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,63 @@
+# Leitura de um número inteiro
+numero = int(input("Digite um número inteiro: "))
+
+# Cálculo do quadrado
+quadrado = numero ** 2
+
+# Apresentação do resultado
+print(f"O quadrado de {numero} é {quadrado}.")
+
+
+
+# Criando variáveis para primeiro nome e sobrenome
+primeiro_nome = "Rayssa"
+sobrenome = "Thereza"
+
+# Concatenando para formar o nome completo
+nome_completo = primeiro_nome + " " + sobrenome
+
+# Exibindo o nome completo
+print("Nome completo:", nome_completo)
+
+
+
+# Solicita dois números inteiros ao usuário
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+
+# Converte os números para strings e concatena
+resultado = str(num1) + str(num2)
+
+# Exibe o resultado da concatenação
+print("Resultado da concatenação:", resultado)
+
+
+# Armazena a palavra "Python"
+linguagem = "Python"
+
+# Número inteiro a ser adicionado
+versao = 3
+
+# Concatenação (convertendo o número para string)
+resultado = linguagem + str(versao)
+
+# Exibe o resultado
+print("Resultado da concatenação:", resultado)
+
+
+
+
+
+# Declara uma variável com uma frase
+frase = "Hoje eu estou aprendendo sobre "
+
+# Solicita ao usuário uma palavra
+palavra = input("Digite uma palavra para completar a frase: ")
+
+# Concatena a palavra no final da frase
+frase_completa = frase + palavra
+
+# Exibe o resultado final
+print("Frase completa:", frase_completa)
+
+
